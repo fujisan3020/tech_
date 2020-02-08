9df0c0f6774eb1c7fcf6e9599b376680e8cfa4b5
fujisan <gashigashi3040@gmail.com>
2020-02-08 22:46:49 +0900

    add Routing
    Task

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+  Route::get('news/create', 'Admin\NewsController@add');
+
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\profileController@edit');
+});
+
+Route::get('', '\AAAController@bbb');
