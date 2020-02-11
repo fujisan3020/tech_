441942fbd7f2bfcf5f217a875ddc1cfaf42b151f
fujisan <gashigashi3040@gmail.com>
2020-02-12 00:00:26 +0900

    課題提出14

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,50 @@
 @extends('layouts.profile')
 
-@section('title', 'Myプロフィール')
+@section('title', 'Myプロフィール作成')
 
 @section('content')
       <div class="container">
          <div class="row">
             <div class="col-md-8 mx-auto">
-               <h2>Myプロフィール</h2>
+               <h2>Myプロフィール作成</h2>
+               <form action="{{ action('Admin\profileController@create') }}" method="post">
+
+                   @if (count($errors) > 0)
+                     <ul>
+                        @foreach ($errors->all() as $e)
+                             <li>{{ $e }}</li>
+                        @endforeach
+                     </ul>
+                   @endif
+                   <div class="form-group row">
+                      <label class="col-md-2" for="name">氏名</label>
+                      <div class="col-md-10">
+                         <input class="form-control" type="text"  name="name">{{ old('name') }}
+                      </div>
+                   </div>
+                   <div class="form-group row">
+                      <label class="col-md-2" for="sex">性別</label>
+                      <div class="col-md-10">
+                        <input class="form-control" type="text" name="sex">{{ old('sex') }}
+                      </div>
+                   </div>
+                   <div class="form-group row">
+                      <label class="col-md-2" for="hobby">趣味</label>
+                      <div class="col-md-10">
+                        <textarea class="form-control" name=="hobby" rows="20">{{ old('hobby') }}</textarea>
+                      </div>
+                   </div>
+                   <div class="form-group row">
+                      <label class="col-md-2" for="introduction">自己紹介欄</label>
+                      <div class="col-md-10">
+                        <textarea class="form-control" name="introduction" rows="20" role="20">
+                          {{ old('introduction') }}</textarea>
+                      </div>
+                   </div>
+
+                   {{ csrf_field() }}
+                   <input class="btn btn-primary" type="submit" value="更新">
+               </form>
             </div>
          </div>
       </div>
